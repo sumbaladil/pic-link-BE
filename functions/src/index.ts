2474c34b38de9900fb12d88cac5d45500bb3056d
mikemorancodes <mikemoranmusic@gmail.com>
2018-05-29 22:48:44 +0100

    testing db writes on user creation.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,39 @@
-import * as functions from 'firebase-functions';
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const db = admin.firestore();
+admin.initializeApp();
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// exports.createUserInDB = functions.auth.user().onCreate(user => {
+//   if (user.data().photoURL) {
+//     db.ref('enrolledFaces').doc(user.data().uid).then
+//   }
+// });
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// exports.updateUserMatchedImages = functions.storage.object().onFinalize(image => {
+//   const { uid } = image.data.metadata.uid;
+
+// });
+
+const userCreate = functions.auth.user().onCreate((user, context) => {
+  const db = admin.firestore();
+  return db.runTransaction(async transaction => {
+    const userId = db.doc(`user/${user.uid}`);
+    transaction.set(userId, {
+      displayName: user.displayName,
+      email: user.email,
+      phone: user.phoneNumber || ""
+    });
+
+    const uacId = db.doc(`user_access_control/${user.uid}`);
+    transaction.set(uacId, {
+      permissions: {}
+    });
+
+    return {
+      userId,
+      uacId
+    };
+  });
 });
+
+export default userCreate;
